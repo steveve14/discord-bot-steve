86e9fb2b360df126e582dad2d8d13cb9ebeb6b0a
steveve14 <46221122+steveve14@users.noreply.github.com>
2020-10-18 20:26:14 +0900

    Update steve-bot-test.py

--- steve-bot-test.py
+++ steve-bot-test.py
@@ -16,13 +16,16 @@ async def on_ready():
     print('------')
     game = discord.Game("steve bot 실행")
     await client.change_presence(status=discord.Status.online, activity=game)
+    
 @client.command(name="청소", pass_context=True)
 async def _clear(ctx, *, amount=5):
     await ctx.channel.purge(limit=amount)
     pass    
+
 @client.command(name="list")
 async def _list(ctx, arg):
     pass
+
 @client.event
 async def on_massage(message):
     await client.process_commands(message)
