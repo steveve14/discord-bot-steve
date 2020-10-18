8987058a2407f8678170d55bf2308113c75a3012
steveve14 <46221122+steveve14@users.noreply.github.com>
2020-10-18 20:25:26 +0900

    Update steve-bot-test.py

--- steve-bot-test.py
+++ steve-bot-test.py
@@ -54,5 +54,6 @@ async def on_massage(message):
             await message.channel.send("숫자로 넣어주세요.")
         except ZeroDivisionError:
             await message.channel.send("You can't divide with 0.")
-    access_token = os.environ["BOT_TOKEN"]
+            
+    access_token = os.environ['BOT_TOKEN']
     client.run(access_token)
